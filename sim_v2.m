cfd427e8a2c5f6f7c7ccd0b4cbafa39d0b0c75f8
micaelcoelho <96194104+micaelcoelho@users.noreply.github.com>
2022-06-11 20:14:35 +0100

    Update sim_v2.m
    Interface implementada

--- sim_v2.m
+++ sim_v2.m
@@ -1,15 +1,18 @@
-clear all
+function [] = sim_v4(alpha1,alpha2,alpha3,beta1,beta2,beta3,ponto1,ponto2,ponto3,lidarVector,lidarPoint)
 close all
 
 
 %lidarVec=[0 -1 -0.1];
-axisrot=[0.2 0.1 1]; % eixo sobre o qual se rotaciona o 1º vetor do lidar
+%axisrot=[0.2 0.1 1]; % eixo sobre o qual se rotaciona o 1º vetor do lidar
+axisrot=Conv(lidarVector);
 axisrotH=[axisrot 1];
 lidarVec=makehgtform('xrotate',deg2rad(90))*axisrotH';
 
 
 lidarPack=lidarVec(1:3)';
-pointLidar=[5,5,5];
+%pointLidar=[5,5,5];
+pointLidar=Conv(lidarPoint);
+
 
 quiver3(pointLidar(1),pointLidar(2),pointLidar(3),lidarPack(1),lidarPack(2),lidarPack(3),20); % plot
 xlabel('X');
@@ -34,9 +37,13 @@ lidarPack=lidarPack(2:end,:);
 
 %%
 % definição do plano de refleção inicial
-a=[0 -2 4];
-b=[5 0 0];
-p=[10 -5 5];
+%a=[0 -2 4]; 
+%b=[5 0 0];
+%p=[10 -5 5];
+
+a=Conv(alpha1);
+b=Conv(beta1);
+p=Conv(ponto1);
 
 [xx,yy,zz]=planePoint(p(1),p(2),p(3),a(1),a(2),a(3),b(1),b(2),b(3));
 surf(xx,yy,zz);
@@ -86,9 +93,13 @@ for i=1:size(lidarPack,1)
 end
 %%
 % definição do plano de refleção secundario
-a1=[0 5 0];
-b1=[5 0 0];
-p1=[10 -5 15];
+%a1=[0 5 0];
+%b1=[5 0 0];
+%p1=[10 -5 15];
+
+a1=Conv(alpha2);
+b1=Conv(beta2);
+p1=Conv(ponto2);
 
 [xx1,yy1,zz1]=planePoint(p1(1),p1(2),p1(3),a1(1),a1(2),a1(3),b1(1),b1(2),b1(3));
 surf(xx1,yy1,zz1);
@@ -131,9 +142,13 @@ end
 
 %%
 % definição do plano que representa o objeto
-a2=[0 0 4];
-b2=[5 0 5];
-p2=[10 10 5];
+%a2=[0 0 4];
+%b2=[5 0 5];
+%p2=[10 10 5];
+
+a2=Conv(alpha3);
+b2=Conv(beta3);
+p2=Conv(ponto3);
 
 [xx2,yy2,zz2]=planePoint(p2(1),p2(2),p2(3),a2(1),a2(2),a2(3),b2(1),b2(2),b(3));
 surf(xx2,yy2,zz2);
@@ -152,4 +167,5 @@ for i=1:size(lidarPack,1)
         plot3(I2(1),I2(2),I2(3),'o');
         hold on
     end
+end
 end
